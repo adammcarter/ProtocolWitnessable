ee4f254026b7b20eb7546a55d7754111c5f6bf31
Adam Carter <14096630+adamcarter93@users.noreply.github.com>
2024-03-24 20:29:13 +0000

    Added explicit names for attached extension macro

--- Sources/ProtocolWitnessing/ProtocolWitnessing.swift
+++ Sources/ProtocolWitnessing/ProtocolWitnessing.swift
@@ -3,5 +3,5 @@
 
 /// A macro that produces a protocol witness from the types functions
 @attached(peer, names: suffixed(ProtocolWitness))
-@attached(extension)
+@attached(extension, names: named(makeErasedProtocolWitness), named(makingProtocolWitness))
 public macro ProtocolWitnessing() = #externalMacro(module: "ProtocolWitnessingMacros", type: "WitnessingMacro")
